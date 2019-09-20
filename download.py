0a561aa49221f246aa54b22954f828b5e3538d04
ewauq <emilien.wauquier@gmail.com>
2019-09-20 10:43:56 +0200

    Updated code for python 2 & macos

--- download.py
+++ download.py
@@ -1,20 +1,28 @@
+# coding: utf8
+
 import os
 
-cmd = 'py bin/youtube-dl'
+if os.name == "posix":
+  py = "python"
+else:
+  py = "py"
+
+cmd = py + " bin/youtube-dl"
 opt = '-f "bestvideo[height<=1080]+bestaudio[ext=m4a]/best[ext=mp4]" --merge-output-format mp4 --output "videos/%(title)s [%(resolution)s].%(ext)s" --restrict-filenames'
 
-print ("----------------------------------------------")
-print ("> Lancement du téléchargement des vidéos")
-print ("----------------------------------------------")
+print "----------------------------------------------"
+print "> Lancement du téléchargement des vidéos"
+print "----------------------------------------------"
 
 filepath = 'list.txt'
 count = len(open(filepath).readlines())
 
 with open(filepath) as fp:
   for cnt, line in enumerate(fp):
-    print(f'\033[0;36;40m> [{cnt+1}/{count}] Téléchargement de {line}\033[0m \n> {cmd} {opt} {line}')
-    os.system(f'{cmd} {opt} {line}')
+    print "\033[0;36;40m> [",cnt + 1,"/", count, "] Téléchargement de ", line,"\033[0m \n> ", cmd, " ", opt, " ", line
+    os.system(cmd + " " + opt + " " + line)
+    print "\n\033[0;32;40mTéléchargement terminé.\033[0m\n"
 
-print ("----------------------------------------------")
-print ("> Téléchargement des vidéos terminé.")
-print ("----------------------------------------------")
+print "----------------------------------------------"
+print "> Téléchargement des vidéos terminé."
+print "----------------------------------------------"
